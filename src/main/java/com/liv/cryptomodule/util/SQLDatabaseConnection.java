fbf7ee26b8eadfb6485cc30e3d521b0374722522
Alexey Shapoval <as@eubclab.com>
2021-03-24 15:58:08 +0200

    Hotfix connection

--- src/main/java/com/liv/cryptomodule/util/SQLDatabaseConnection.java
+++ src/main/java/com/liv/cryptomodule/util/SQLDatabaseConnection.java
@@ -372,13 +372,13 @@ public class SQLDatabaseConnection {
                 query.append(prop.getProperty(USER_TABLE)).append(" SET ");
                 break;
             case 1:
-                query.append(prop.getProperty(NOTARY_TABLE)).append(" SET ");
-                query.append("public_key").append(DSM.encodePK(DSM.generateKeyPair(user.getPassword().getBytes(StandardCharsets.UTF_8)).getPublic())).append(","
+                query.append(prop.getProperty(USER_TABLE)).append(" SET ");
+                query.append("public_key=\"").append(DSM.encodePK(DSM.generateKeyPair(user.getPassword().getBytes(StandardCharsets.UTF_8)).getPublic())).append("\","
                 ).append("did=\"").append(did).append("\",");
                 break;
             case 2:
-                query.append(prop.getProperty(REGISTRY_TABLE)).append(" SET ");
-                query.append("public_key").append(DSM.encodePK(DSM.generateKeyPair(user.getPassword().getBytes(StandardCharsets.UTF_8)).getPublic())).append(","
+                query.append(prop.getProperty(USER_TABLE)).append(" SET ");
+                query.append("public_key=\"").append(DSM.encodePK(DSM.generateKeyPair(user.getPassword().getBytes(StandardCharsets.UTF_8)).getPublic())).append("\","
                 ).append("did=\"").append(did).append("\",");
                 break;
             default:
